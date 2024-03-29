973088a2eb91654d6a03fa530a4791897638b276
Владимир Короткий <fess28051988@gmail.com>
2024-03-29 13:33:29 +0300

    1111

--- main_constant.py
+++ main_constant.py
@@ -1,18 +1,32 @@
 from IMemory import IMemoryManager
+from VariablePartitionMemoryManager import VariablePartitionMemoryManager
 from ConstantPartitionMemoryManager import ConstantPartitionMemoryManager
+from PagingMemoryManager import PagingMemoryManager
 import threading
 import random
 import time
 from process import Process
 
-memoryManager = ConstantPartitionMemoryManager(64, 8)
+#memoryManagerVar = VariablePartitionMemoryManager(64, False)
+#memoryManagerVarWithCompress = VariablePartitionMemoryManager(64, True)
+memoryManager = IMemoryManager
 count_worked_process = 0
 count_working_process = 0
+isTheEnd = False
 
 def access_resource(timer, process):    
+    #global memoryManager, count_worked_process, count_working_process   
     global memoryManager, count_worked_process, count_working_process   
+    count_working_process +=1
     # выполняем обработку
-    time.sleep(timer)    
+    time.sleep(timer) 
+    #процесс уснул
+    process.status = 2   
+    time.sleep(timer / 2)
+    #процесс проснулся
+    process.status = 1
+    memoryManager.wakeup_process(process)
+    time.sleep(timer / 2)
     #print(f'{process.id} закончил выполнение. Выгружаем память...') 
     memoryManager.release_memory(process)
     #print(f'{process.id} освободил память.')
@@ -20,10 +34,19 @@ def access_resource(timer, process):
     count_working_process -= 1
 
 def main_thread(all_task):
-    global memoryManager, count_working_process    
+    global memoryManager, count_working_process, count_worked_process, isTheEnd, count_second
+    processes = list()     
     for i in range(all_task):
-        process = Process(random.randint(1,8))
-        #print(f'{process.id} пробует получить память для загрузки')    
+        process = Process(random.randint(2,6))
+        processes.append(process)
+        #print(f'{process.id} пробует получить память для загрузки')   
+
+    #проверка менеджера памяти с постоянными разделами
+    memoryManager = ConstantPartitionMemoryManager(64,8, False)
+    count_working_process = 0
+    count_worked_process = 0 
+    count_second = 0
+    for process in processes:
         while True:
             memoryManager.allocate_memory(process)
             if process.countSpaces == 0:
@@ -32,17 +55,20 @@ def main_thread(all_task):
             process.status = 1
             break
         #print(f'{process.id} получил память для загрузки. Начинаем выполнение...') 
-        count_working_process +=1
         thread = threading.Thread(target=access_resource, daemon=True, args=(random.randint(5,10), process, ))
-        thread.start()
+        thread.start()   
+    while count_working_process != 0:
+        time.sleep(1)
+    time.sleep(5)
+
 
 
 #создадим процессы
-all_task = 100
+all_task = 50
 thread = threading.Thread(target=main_thread, daemon=True, args=(all_task, ))
 thread.start()
 count_second = 0
-while count_worked_process != all_task:
+while isTheEnd == False:
     time.sleep(1)
     count_second += 1
     print(memoryManager.get_status())
